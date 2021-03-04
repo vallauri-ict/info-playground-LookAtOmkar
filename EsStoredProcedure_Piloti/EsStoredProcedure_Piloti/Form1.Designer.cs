273fa3c815080f8f976cbe1fa90c99cc0500beb6
LookAtOmkar <o.rathore.00773@vallauri.edu>
2021-03-04 10:43:40 +0100

    new es

---
+++ EsStoredProcedure_Piloti/EsStoredProcedure_Piloti/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace EsStoredProcedure_Piloti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(33, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(619, 250);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(33, 21);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(192, 23);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "STORED PROCEDURE";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn;
+    }
+}
+
---
+++ EsStoredProcedure_Piloti/EsStoredProcedure_Piloti/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace EsStoredProcedure_Piloti
+{
+    public partial class Form1 : Form
+    {
+        public static string WORKING_PATH = @"C:\Users\Rathore\GitHub - Projects\info-playground-LookAtOmkar\EsStoredProcedure_Piloti\ES_StoredProcedure_Piloti.mdf";
+        public static string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="+WORKING_PATH+";Integrated Security = True; Connect Timeout = 30";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection())
+            {
+                string sql = "ES1";
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                SqlParameter teamcode = new SqlParameter();
+                teamcode.ParameterName = "code";
+                teamcode.Direction = ParameterDirection.Input;
+                teamcode.DbType = DbType.String; // Tipo di dato
+                teamcode.Size = 50;  //Dimensione del dato
+                teamcode.Value = "IT";
+                cmd.Parameters.Add(teamcode);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                dataGridView1.DataSource = dt;
+
+                MessageBox.Show("Result--->  ok" );
+            }
+           
+        }
+    }
+}
